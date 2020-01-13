a1df9bc9aea77f4252a11661a4055b0982b74ab4
Stijn Vandendriessche <stijn.vandendriessche3@student.howest.be>
2020-01-13 15:54:02 +0100

    test

--- etc/testHeartRate/js/script.js
+++ etc/testHeartRate/js/script.js
@@ -80,7 +80,7 @@ const init = function()
     {
         bluetooth(hartslag2, 2);
     });
-    Console.lo("test kraken");
+    Console.lo("test huppeldepup feature");
 };
 
 document.addEventListener("DOMContentLoaded", init);
