b727752615ced6aaf61e119a6b6c1ef74df422a3
Stijn Vandendriessche <stijn.vandendriessche3@student.howest.be>
2020-01-21 10:52:26 +0100

    begin page 4

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,4 +1,4 @@
-let index, gameOver, makey, hartMain;
+let index, gameOver, makey, hartMain, connect;
 
 const setIndex = function() 
 {
@@ -24,13 +24,26 @@ const setMakey = function()
 
 const setHeartMain = function() 
 {
-    //btn = document.querySelector('.js-button-next');
+    btn = document.querySelector('.js-btn-noHeartRate');
+    btnOne = document.querySelector('.js-btn-connectHeartRate');
     bar = document.querySelector('.js-bar-two');
     btn.addEventListener("click", function () {
-        setPage("hart");
+        //setPage("");
+        //hier moet de speler naar het hoofdmenu gebracht worden!
         btn.removeEventListener("click", this);
     });
+    btnOne.addEventListener("click", function() 
+    {
+        setPage("connect");
+        btnOne.removeEventListener("click", this); 
+    });
     bar.style.width = "40%";
+};
+
+const setConnect = function() 
+{
+    bar = document.querySelector('.js-bar-three');
+    bar.style.width = "60%";
 };
 
 const setPage = function(page)
@@ -42,12 +55,14 @@ const setPage = function(page)
             gameOver.style.display = "none";
             makey.style.display = "none";
             hartMain.style.display = "none";
+            connect.style.display = "none";
             break;
         case "index":
             index.style.display = "block";
             gameOver.style.display = "none";
             makey.style.display = "none";
             hartMain.style.display = "none";
+            connect.style.display = "none";
             setIndex();
             break;
         case "gameOver":
@@ -55,12 +70,14 @@ const setPage = function(page)
             gameOver.style.display = "block";
             makey.style.display = "none";
             hartMain.style.display = "none";
+            connect.style.display = "none";
             break;
         case "makey":
             index.style.display = "none";
             gameOver.style.display = "none";
             makey.style.display = "block";
             hartMain.style.display = "none";
+            connect.style.display = "none";
             setMakey();
             break;
         case "hart":
@@ -68,13 +85,23 @@ const setPage = function(page)
             gameOver.style.display = "none";
             makey.style.display = "none";
             hartMain.style.display = "block";
+            connect.style.display = "none";
             setHeartMain();
+            break;
+        case "connect":
+            index.style.display = "none";
+            gameOver.style.display = "none";
+            makey.style.display = "none";
+            hartMain.style.display = "none";
+            connect.style.display = "block";
+            setConnect();
             break;
         default:
             index.style.display = "none";
             gameOver.style.display = "none";
             makey.style.display = "none";
             hartMain.style.display = "none";
+            connect.style.display = "none";
             break;
     }
 };
@@ -85,6 +112,8 @@ const initialize = function ()
     gameOver = document.querySelector('.js-gameOver');
     makey = document.querySelector('.js-makey');
     hartMain = document.querySelector('.js-hartbeat__main');
+    connect = document.querySelector('.js-connect');
+    console.log(connect);
     setPage("index");
 };
 
